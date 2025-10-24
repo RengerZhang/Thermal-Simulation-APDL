0a8f6280e64d57bea83cd3114e30a9477ab48620
RengerZhang <renger.z@qq.com>
2025-10-24 11:12:37 +0800

    Add a specific APDL startup path

--- .PY/test.py
+++ .PY/test.py
@@ -2,7 +2,7 @@ from ansys.mapdl.core import launch_mapdl
 
 project_root = r"C:\Users\24880\Desktop\Thermal-Simulation-APDL\.MAC\image.mac"
 
-mapdl = launch_mapdl()
+mapdl = launch_mapdl(exec_file="C:/Program Files/ANSYS Inc/v241/ANSYS/bin/winx64/ansys241.exe")
 mapdl.clear()
 mapdl.input(project_root)
 
