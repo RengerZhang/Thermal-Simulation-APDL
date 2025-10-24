b3dff56c7a24740890cee74944eb969a6eac597d
RengerZhang <renger.z@qq.com>
2025-10-24 11:22:56 +0800

    modified:   .PY/test.py

--- .PY/test.py
+++ .PY/test.py
@@ -2,7 +2,7 @@ from ansys.mapdl.core import launch_mapdl
 
 project_root = r"C:\Users\24880\Desktop\Thermal-Simulation-APDL\.MAC\image.mac"
 
-mapdl = launch_mapdl(exec_file="C:/Program Files/ANSYS Inc/v241/ANSYS/bin/winx64/ansys241.exe")
+mapdl = launch_mapdl(exec_file="D:\Program Files\ANSYS Inc\v251\ansys\bin\winx64\ANSYS251.exe")
 mapdl.clear()
 mapdl.input(project_root)
 
